efe6214a890f2f922f8855710eb86a72a5b49878
MD. Faysal Hossain Tibro <93143453+Tibro0@users.noreply.github.com>
2025-11-20 20:48:08 +0600

    Some Update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,7 +24,7 @@ class UserSeeder extends Seeder
                 'address_line_two'=> 'Tongi East, Banomala Road.',
                 'short_description'=> 'Full-stack developer skilled in Laravel, PHP, JavaScript, and modern frameworks. Adept at building dynamic web apps, problem-solving, and clear team collaboration. Dedicated to delivering high-quality, user-focused solutions.',
                 'email_verified_at'=> now(),
-                'password'=> Hash::make('faysaltibro0171226103501966221364'),
+                'password'=> Hash::make('password'),
                 'remember_token'=> Str::random(10),
                 'created_at'=> now(),
                 'updated_at'=> now(),
