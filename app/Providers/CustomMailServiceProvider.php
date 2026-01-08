6c50ff694aa59c8a7d7fb4ccfd19ce39fd09edfc
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-01-08 17:45:32 +0600

    Github Setting Frontend

--- app/Providers/CustomMailServiceProvider.php
+++ app/Providers/CustomMailServiceProvider.php
@@ -23,9 +23,7 @@ class CustomMailServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $mailSetting = Cache::rememberForever('mail_settings', function () {
-            $key = ['site_name', 'mail_driver', 'mail_host', 'mail_port', 'mail_username', 'mail_password', 'mail_from_address', 'mail_receive_address'];
-
-            return Setting::whereIn('key', $key)->pluck('value', 'key')->toArray();
+            return Setting::pluck('value', 'key')->toArray();
         });
 
         if ($mailSetting) {
@@ -36,6 +34,10 @@ class CustomMailServiceProvider extends ServiceProvider
             Config::set('mail.mailers.smtp.username', $mailSetting['mail_username']);
             Config::set('mail.mailers.smtp.password', $mailSetting['mail_password']);
             Config::set('mail.from.address', $mailSetting['mail_from_address']);
+
+            Config::set('services.github.client_id', $mailSetting['github_client_id']);
+            Config::set('services.github.client_secret', $mailSetting['github_client_secret']);
+            Config::set('services.github.redirect', $mailSetting['github_redirect_url']);
         }
     }
 }
