e7ee1b49594d5f6855be7c5e4c3832878703b2d4
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-02-18 23:50:46 +0600

    Github Code Update

--- app/Http/Controllers/Frontend/GithubLoginController.php
+++ app/Http/Controllers/Frontend/GithubLoginController.php
@@ -3,20 +3,35 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 use Laravel\Socialite\Socialite;
 
 class GithubLoginController extends Controller
 {
+    public static function githubApiSetting()
+    {
+        $githubSetting = Setting::pluck('value', 'key')->toArray();
+
+        Config::set('services.github.client_id', $githubSetting['github_client_id']);
+        Config::set('services.github.client_secret', $githubSetting['github_client_secret']);
+        Config::set('services.github.redirect', $githubSetting['github_redirect_url']);
+    }
+
     public function githubLogin()
     {
+        $this->githubApiSetting();
+
         return Socialite::driver('github')->redirect();
     }
 
     public function githubCallback()
     {
+        $this->githubApiSetting();
+
         $githubUser = Socialite::driver('github')->user();
 
         $existingUser = User::where('email', $githubUser->email)->first();
--- app/Providers/CustomMailServiceProvider.php
+++ app/Providers/CustomMailServiceProvider.php
@@ -35,10 +35,6 @@ class CustomMailServiceProvider extends ServiceProvider
             Config::set('mail.mailers.smtp.password', $mailSetting['mail_password']);
             Config::set('mail.from.address', $mailSetting['mail_from_address']);
 
-            Config::set('services.github.client_id', $mailSetting['github_client_id']);
-            Config::set('services.github.client_secret', $mailSetting['github_client_secret']);
-            Config::set('services.github.redirect', $mailSetting['github_redirect_url']);
-
             Config::set('services.google.client_id', $mailSetting['google_client_id']);
             Config::set('services.google.client_secret', $mailSetting['google_client_secret']);
             Config::set('services.google.redirect', $mailSetting['google_redirect_url']);
